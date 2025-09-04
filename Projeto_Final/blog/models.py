164bd2c82f91cbc14cb1f3c5b28ca41552bce802
Gustav Hilbert <103065280+GusHilbert@users.noreply.github.com>
2025-09-03 21:46:14 -0300

    Aula13-03.09.25

--- Projeto_Final/blog/models.py
+++ Projeto_Final/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Tag(models.Model):
     nome = models.CharField(max_length=50, unique=True)
@@ -15,15 +16,14 @@ class Categoria(models.Model):
         return self.nome
 
 
-class Produto(models.Model):
-    nome = models.CharField(max_length=150)
-    descricao = models.TextField(blank=True)
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
-    quantidade = models.PositiveIntegerField(default=0)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='produtos')
+class Artigo(models.Model):
+    titulo = models.CharField(max_length=200)
+    conteudo = models.TextField()
+    autor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, related_name='artigos')
     tags = models.ManyToManyField(Tag, blank=True)
-    criado_em = models.DateTimeField(auto_now_add=True)
+    publicado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.nome
+        return self.titulo
